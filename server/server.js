684c140f09168412c53942e7295c9087d8934a83
jj <jjaywallace@gmail.com>
2022-05-05 12:21:03 -0700

    adding 404, routes, error handling and npm logger Morgan

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import logger from 'morgan';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -7,6 +8,30 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
    console.log(`Server listening on port ${PORT}`)
+});
+
+//Lets do some logs
+app.use(logger('dev', {
+   skip: () => app.get('env') === 'test',
+ }));
+
+// Routes
+app.use('/', routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Route Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, _req, res) => {
+  res
+    .status(err.status || 500)
+    .render('error', {
+      message: err.message,
+    });
 });
 
 export default app;
